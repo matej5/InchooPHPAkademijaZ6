44f9cca9c8f05991ee5a506296d19411e83c8410
matej5 <30931414+matej5@users.noreply.github.com>
2019-02-05 02:59:54 +0100

    Add files via upload
    script from server with latest inputs

--- script.sql
+++ script.sql
@@ -1,21 +1,81 @@
-DROP DATABASE IF EXISTS social_network;
-CREATE DATABASE social_network CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-use social_network;
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: polaznik23_mvc
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
 
-create table post(
-id int not null primary key auto_increment,
-dateCreated timestamp not null default current_timestamp,
-content text,
-image text
-)engine=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `comment`
+--
 
-create table comment(
-id int not null primary key auto_increment,
-postId int not null,
-dateCreated timestamp not null default current_timestamp,
-content text,
-foreign key (postId) references post(id)
-)engine=InnoDB;
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postId` int(11) NOT NULL,
+  `dateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text,
+  PRIMARY KEY (`id`),
+  KEY `postId` (`postId`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `post` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-insert into post (content, image) values ('Evo danas pada kiša opet :(', 'Rain.jpg'), ('Jedem jagode.', 'Strawberry.jpg');
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,2,'2019-02-03 21:41:13','komentar'),(2,1,'2019-02-03 21:43:20','Komentar 2'),(3,2,'2019-02-03 22:19:46','neki post'),(4,2,'2019-02-03 22:19:53','lkjhg'),(5,1,'2019-02-04 00:52:23','ne pada kiša više'),(6,2,'2019-02-04 03:07:41','još neki komentar'),(8,6,'2019-02-04 20:08:52','Komentar 2');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text,
+  `image` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'2019-02-03 20:44:17','Evo danas pada kiša opet :(','Rain.jpg'),(2,'2019-02-03 20:44:17','Jedem jagode.','Strawberry.jpg'),(3,'2019-02-03 20:44:36','asd','YellowLabradorLooking.jpg'),(4,'2019-02-03 22:43:05','Danas sam bio u trgovini!','download.jpg'),(5,'2019-02-04 13:06:56','neki post',NULL),(6,'2019-02-04 13:32:12','Novi post!',NULL),(8,'2019-02-04 14:48:42','Test post',NULL);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-05  1:55:32
