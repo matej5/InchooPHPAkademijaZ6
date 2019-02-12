6bd2fc70281d38805384993622814478ae19fbfa
matej5 <30931414+matej5@users.noreply.github.com>
2019-02-12 21:52:14 +0100

    something went wrong last push

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'url' => 'http://polaznik23mvc.inchoo4u.net/',
+    'mode'  => 'development',
+    "db"=>[
+        "host"=>"localhost",
+        "name"=>"social_network",
+        "user"=>"polaznik23",
+        "password"=>"Inc00PAk."
+    ]
+];
